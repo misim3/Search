295a39171e797b3f686b051e30ad1c6beef77f52
misim3 <sim000314@naver.com>
2024-05-24 13:23:59 +0900

    refactor: 레포지토리 내 메소드 추가

--- src/main/java/com/example/search/repository/BusStopRepository.java
+++ src/main/java/com/example/search/repository/BusStopRepository.java
@@ -2,6 +2,7 @@ package com.example.search.repository;
 
 import com.example.search.domain.Point;
 import com.example.search.entity.BusStop;
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -15,4 +16,6 @@ public interface BusStopRepository extends JpaRepository<BusStop, Long> {
 
     @Query("SELECT b FROM BusStop b WHERE b.longitude BETWEEN :x1 AND :x2 AND b.latitude BETWEEN :y1 AND :y2")
     List<BusStop> findAllByRange(double x1, double x2, double y1, double y2);
+
+    BusStop findByCityCodeAndNodeId(Integer cityCode, String nodeId);
 }
--- src/main/java/com/example/search/repository/CarRepository.java
+++ src/main/java/com/example/search/repository/CarRepository.java
@@ -15,4 +15,8 @@ public interface CarRepository extends JpaRepository<Car, Long> {
 
     @Query("SELECT c FROM Car c WHERE c.longitude BETWEEN :x1 AND :x2 AND c.latitude BETWEEN :y1 AND :y2")
     List<Car> findAllByRange(@Param("x1") double x1,@Param("x2") double x2,@Param("y1") double y1,@Param("y2") double y2);
+
+    Boolean existsByStartNodeId(String startNodeId);
+
+    List<Car> findAllByStartNodeId(String startNodeId);
 }
